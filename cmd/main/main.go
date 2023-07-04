e5658fa013945e45e0e524de3a7a952ca1587d2d
Emir Emre Sahna <emre.sahna@hotmail.com>
2023-07-04 21:56:11 +0300

    init

---
+++ cmd/main/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/EmreSahna/go_mysql_book_management/pkg/routes"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
