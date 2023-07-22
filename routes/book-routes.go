cc7afefe0500cfa2f154490fb27e90a749e94bd6
emresahna <emre.sahna@hotmail.com>
2023-07-22 17:53:53 +0300

    hotfix: endpoint names changed

--- routes/book-routes.go
+++ routes/book-routes.go
@@ -1,14 +1,14 @@
 package routes
 
 import (
-	"github.com/EmreSahna/go_mysql_book_management/controllers"
+	"github.com/EmreSahna/go_mysql_book_management/handlers"
 	"github.com/gorilla/mux"
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
-	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
-	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book", handlers.CreateBook).Methods("POST")
+	router.HandleFunc("/book", handlers.GetBooks).Methods("GET")
+	router.HandleFunc("/book/{bookId}", handlers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", handlers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", handlers.DeleteBook).Methods("DELETE")
 }
--- routes/user-routes.go
+++ routes/user-routes.go
@@ -1,11 +1,11 @@
 package routes
 
 import (
-	"github.com/EmreSahna/go_mysql_book_management/controllers"
+	"github.com/EmreSahna/go_mysql_book_management/handlers"
 	"github.com/gorilla/mux"
 )
 
 var RegisterUserRoutes = func(router *mux.Router) {
-	router.HandleFunc("/user/", controllers.CreateUser).Methods("POST")
-	router.HandleFunc("/user/find", controllers.GetUserByUsernameAndPassword).Methods("POST")
+	router.HandleFunc("/user", handlers.CreateUser).Methods("POST")
+	router.HandleFunc("/user/find", handlers.GetUserByUsernameAndPassword).Methods("POST")
 }
